d81418ebae9dd7a4f51ccbf1cbfc0180abfdb5df
unknown <lilyaharun@gmail.com>
2022-08-08 02:10:27 +0300

    heroku

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 ckeditor = CKEditor(app)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mtbbwavkisckur:8dc772ec62f5cf329ae42fbc5885e406e16fb793330348ade44f9ac170251c4b@ec2-44-193-178-122.compute-1.amazonaws.com:5432/de4rc4jtp68tic'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cvpkqasvmskpco:bbe549c556bc92f2b3b1f47f1929773ae94b5dffc5253ab35336fe8267d7c815@ec2-44-195-100-240.compute-1.amazonaws.com:5432/d6rop4uv3f108o'
 
 app.config['SECRET_KEY'] = "my secret key"
 
